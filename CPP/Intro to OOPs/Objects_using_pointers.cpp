7913f018fdb1de40a3599b3e3e0cb2aa32241f15
Roshan Limbu <topazkayle@gmail.com>
2023-10-23 16:15:08 +0545

    fixed memory leaks

--- CPP/Intro to OOPs/Objects_using_pointers.cpp
+++ CPP/Intro to OOPs/Objects_using_pointers.cpp
@@ -1,29 +1,22 @@
+#include <algorithm>
 #include <iostream>
 using namespace std;
-class Rectangle
-{
+class Rectangle {
 public:
-    int length, breadth;
-    int area()
-    {
-        return length * breadth;
-    }
-    int Perimeter();
+  int length, breadth;
+  int area() { return length * breadth; }
+  int Perimeter();
 };
-int Rectangle::Perimeter()
-{
-    return 2 * (length + breadth);
-}
+int Rectangle::Perimeter() { return 2 * (length + breadth); }
 
-int main()
-{
-    Rectangle r;
-
-    Rectangle *p = new Rectangle();
-    // r.length = 10;
-    p->length = 10;
-    p->breadth = 5;
-    cout << p->area();
-    cout << p->Perimeter();
-    return 0;
+int main() {
+  Rectangle r;
+  Rectangle *p = new Rectangle();
+  
+  p->length = 10;
+  p->breadth =5;
+  cout << p-> area() <<endl;
+  cout << p->Perimeter() << endl;
+  delete p; // delocating memory here for mem leaks issue
+  return 0;
 }
