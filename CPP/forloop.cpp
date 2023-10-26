6509b307b233dcbb0062cc1e73910cb7033e19b1
Sulaiman Butt <sulaimanbutt1002005@gmail.com>
2023-10-27 00:26:38 +0500

    Issue #24 solved

---
+++ CPP/forloop.cpp
@@ -0,0 +1,19 @@
+/*A for loop is a control flow statement that is used to execute a piece of code 
+based on the validity of some conditions. It makes the code execute repeatedly. 
+
+It is used when we already know how many iterations will take place
+*/
+
+#include <iostream>
+
+using namespace std;
+
+int main ()
+
+// Displays first 10 digits
+{
+    for (int i = 0; i <= 10; i = i + 2)
+     {
+  cout << i << "\n";
+    }
+}
