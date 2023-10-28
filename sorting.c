91806c0e79289cf03cb8b2ad4914540d60159e6e
Suprabha24 <128732845+Suprabha24@users.noreply.github.com>
2023-10-28 09:45:59 +0530

    Sorting in C
    Sorting in C using Bubble Sort

---
+++ sorting.c
@@ -0,0 +1,49 @@
+#include <stdio.h>
+
+void bubbleSort(int arr[], int n) {
+    int temp;
+    int swapped;
+
+    for (int i = 0; i < n - 1; i++) {
+        swapped = 0; // Initialize the swapped flag to 0
+
+        for (int j = 0; j < n - i - 1; j++) {
+            if (arr[j] > arr[j + 1]) {
+                // Swap arr[j] and arr[j+1]
+                temp = arr[j];
+                arr[j] = arr[j + 1];
+                arr[j + 1] = temp;
+                swapped = 1; // Set the swapped flag to 1
+            }
+        }
+
+        // If no two elements were swapped in the inner loop, the array is already sorted
+        if (swapped == 0) {
+            break;
+        }
+    }
+}
+
+int main() {
+    int n;
+
+    printf("Enter the number of elements: ");
+    scanf("%d", &n);
+
+    int arr[n];
+
+    printf("Enter %d elements:\n", n);
+    for (int i = 0; i < n; i++) {
+        scanf("%d", &arr[i]);
+    }
+
+    bubbleSort(arr, n);
+
+    printf("Sorted array:\n");
+    for (int i = 0; i < n; i++) {
+        printf("%d ", arr[i]);
+    }
+    printf("\n");
+
+    return 0;
+}
